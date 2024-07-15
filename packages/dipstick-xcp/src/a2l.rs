1382a4f7689d7df57e40e201bd6b9233a8a84659
Simon Berger <simon.berger@b-on.com>
2024-07-15 22:16:44 +0200

    add more a2l stuff

--- packages/dipstick-xcp/src/a2l.rs
+++ packages/dipstick-xcp/src/a2l.rs
@@ -2,7 +2,8 @@ use std::sync::{Arc, Mutex};
 
 use dipstick_core::{Core, Entity, EntityKind, EntityMeta};
 use dipstick_proto::xcp::v1::{
-    A2lDataType, A2lEntity, A2lFile, A2lMeasurement, A2lModule, A2lProject, A2lSpec, A2lStatus,
+    A2lAddrType, A2lByteOrder, A2lCharacteristic, A2lDataType, A2lEntity, A2lFile, A2lFncValues,
+    A2lMeasurement, A2lModule, A2lProject, A2lRecordLayout, A2lSpec, A2lStatus,
 };
 use tonic::{Result, Status};
 
@@ -118,16 +119,64 @@ fn map_file(a2l: a2lfile::A2lFile) -> A2lFile {
 }
 
 fn map_module(module: a2lfile::Module) -> A2lModule {
-    let measurement = module
-        .measurement
-        .into_iter()
-        .map(map_measurement)
-        .collect();
-    A2lModule { measurement }
+    A2lModule {
+        characteristic: module
+            .characteristic
+            .into_iter()
+            .map(map_characteristic)
+            .collect(),
+        measurement: module
+            .measurement
+            .into_iter()
+            .map(map_measurement)
+            .collect(),
+        record_layout: module
+            .record_layout
+            .into_iter()
+            .map(map_record_layout)
+            .collect(),
+    }
+}
+
+fn map_characteristic(c: a2lfile::Characteristic) -> A2lCharacteristic {
+    A2lCharacteristic {
+        name: c.name,
+        long_identifier: c.long_identifier,
+        address: c.address,
+        deposit: c.deposit,
+        conversion: c.conversion,
+        byte_order: map_byte_order(c.byte_order.map(|order| order.byte_order)) as _,
+        ecu_address_extension: c.ecu_address_extension.map(|ext| i32::from(ext.extension)),
+    }
+}
+
+fn map_byte_order(byte_order: Option<a2lfile::ByteOrderEnum>) -> A2lByteOrder {
+    match byte_order {
+        Some(a2lfile::ByteOrderEnum::LittleEndian) => A2lByteOrder::LittleEndian,
+        Some(a2lfile::ByteOrderEnum::BigEndian) => A2lByteOrder::BigEndian,
+        Some(a2lfile::ByteOrderEnum::MsbLast) => A2lByteOrder::MsbLast,
+        Some(a2lfile::ByteOrderEnum::MsbFirst) => A2lByteOrder::MsbFirst,
+        Some(a2lfile::ByteOrderEnum::MsbFirstMswLast) => A2lByteOrder::MsbFirstMswLast,
+        Some(a2lfile::ByteOrderEnum::MsbLastMswFirst) => A2lByteOrder::MsbLastMswFirst,
+        None => A2lByteOrder::Unspecified,
+    }
 }
 
 fn map_measurement(meas: a2lfile::Measurement) -> A2lMeasurement {
-    let datatype = match meas.datatype {
+    A2lMeasurement {
+        name: meas.name,
+        long_identifier: meas.long_identifier,
+        datatype: map_data_type(meas.datatype) as _,
+        conversion: meas.conversion,
+        ecu_address: meas.ecu_address.map(|addr| addr.address),
+        ecu_address_extension: meas
+            .ecu_address_extension
+            .map(|ext| i32::from(ext.extension)),
+    }
+}
+
+fn map_data_type(datatype: a2lfile::DataType) -> A2lDataType {
+    match datatype {
         a2lfile::DataType::Ubyte => A2lDataType::Ubyte,
         a2lfile::DataType::Sbyte => A2lDataType::Sbyte,
         a2lfile::DataType::Uword => A2lDataType::Uword,
@@ -139,14 +188,30 @@ fn map_measurement(meas: a2lfile::Measurement) -> A2lMeasurement {
         a2lfile::DataType::Float16Ieee => A2lDataType::F16Ieee,
         a2lfile::DataType::Float32Ieee => A2lDataType::F32Ieee,
         a2lfile::DataType::Float64Ieee => A2lDataType::F64Ieee,
-    };
-    A2lMeasurement {
-        name: meas.name,
-        long_identifier: meas.long_identifier,
-        datatype: datatype as _,
-        ecu_address: meas.ecu_address.map(|addr| addr.address),
-        ecu_address_extension: meas
-            .ecu_address_extension
-            .map(|ext| i32::from(ext.extension)),
+    }
+}
+
+fn map_record_layout(record_layout: a2lfile::RecordLayout) -> A2lRecordLayout {
+    A2lRecordLayout {
+        name: record_layout.name,
+        fnc_values: record_layout.fnc_values.map(map_fnc_values),
+    }
+}
+
+fn map_fnc_values(fnc_values: a2lfile::FncValues) -> A2lFncValues {
+    A2lFncValues {
+        position: u32::from(fnc_values.position),
+        datatype: map_data_type(fnc_values.datatype) as _,
+        address_type: map_addr_type(fnc_values.address_type) as _,
+    }
+}
+
+fn map_addr_type(addr_type: a2lfile::AddrType) -> A2lAddrType {
+    match addr_type {
+        a2lfile::AddrType::Pbyte => A2lAddrType::Pbyte,
+        a2lfile::AddrType::Pword => A2lAddrType::Pword,
+        a2lfile::AddrType::Plong => A2lAddrType::Plong,
+        a2lfile::AddrType::Plonglong => A2lAddrType::Plonglong,
+        a2lfile::AddrType::Direct => A2lAddrType::Direct,
     }
 }
