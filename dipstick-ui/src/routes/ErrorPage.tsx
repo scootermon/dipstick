d25b387183095a4faf18592030c78a7034ef5147
Simon Berger <simon.berger@inomotech.com>
2024-11-01 12:59:28 +0100

    update deps stuff

--- dipstick-ui/src/routes/ErrorPage.tsx
+++ dipstick-ui/src/routes/ErrorPage.tsx
@@ -1,15 +1,26 @@
-import { useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
+
+  let errorMessage;
+  if (isRouteErrorResponse(error)) {
+    errorMessage = error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === "string") {
+    errorMessage = error;
+  } else {
+    console.error(error);
+    errorMessage = "Unknown error";
+  }
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{errorMessage}</i>
       </p>
     </div>
   );
--- dipstick-ui/vite.config.ts
+++ dipstick-ui/vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // 2024-11-01: somehow Firefox doesn't like localhost
+    host: "127.0.0.1",
+  },
 });
