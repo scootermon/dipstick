ac20b3d44dc5620855b667c7c3406a4192c67d69
Simon Berger <simon.berger@inomotech.com>
2024-11-08 17:34:52 +0100

    can: gate linux bus

--- packages/dipstick-can/src/bus.rs
+++ packages/dipstick-can/src/bus.rs
@@ -6,26 +6,28 @@ use tokio::sync::broadcast;
 use tokio_stream::wrappers::BroadcastStream;
 use tonic::{Result, Status};
 
+#[cfg(target_os = "linux")]
 mod linux;
 
 pub struct Bus {
     meta: EntityMeta,
     spec: BusSpec,
-    linux: linux::Bus,
+    variant: Variant,
     tx: broadcast::Sender<Frame>,
 }
 
 impl Bus {
     pub async fn new(core: &Core, meta: EntityMeta, mut spec: BusSpec) -> Result<Arc<Self>> {
         let (tx, _) = broadcast::channel(128); // TODO
-        let linux = match &mut spec.bus_spec_variant {
-            Some(BusSpecVariant::Linux(linux)) => linux::Bus::new(core, linux, tx.clone()).await?,
-            None => return Err(Status::invalid_argument("missing bus spec variant")),
-        };
+        let mut variant_spec = spec
+            .bus_spec_variant
+            .as_mut()
+            .ok_or_else(|| Status::invalid_argument("missing bus spec variant"))?;
+        let variant = Variant::new(core, tx.clone(), &mut variant_spec).await?;
         Ok(Arc::new(Self {
             meta,
             spec,
-            linux,
+            variant,
             tx,
         }))
     }
@@ -48,7 +50,7 @@ impl Bus {
 
     pub async fn send_frame(&self, frame: &Frame) -> Result<()> {
         tracing::trace!(?frame, "sending frame");
-        self.linux.send(frame).await
+        self.variant.send(frame).await
     }
 
     pub fn subscribe(&self) -> BroadcastStream<Frame> {
@@ -65,4 +67,39 @@ impl Entity for Bus {
 impl EntityKind for Bus {
     type Package = crate::Can;
     const KIND: &'static str = "Bus";
+}
+
+enum Variant {
+    #[cfg(target_os = "linux")]
+    Linux(linux::Bus),
+}
+
+impl Variant {
+    async fn new(
+        core: &Core,
+        tx: broadcast::Sender<Frame>,
+        variant_spec: &mut BusSpecVariant,
+    ) -> Result<Self> {
+        match variant_spec {
+            #[cfg(target_os = "linux")]
+            BusSpecVariant::Linux(linux) => {
+                let bus = linux::Bus::new(core, linux, tx.clone()).await?;
+                Ok(Self::Linux(bus))
+            }
+            #[cfg(not(target_os = "linux"))]
+            BusSpecVariant::Linux(_) => {
+                let _ = (core, tx);
+                return Err(Status::invalid_argument("linux bus support not available"));
+            }
+        }
+    }
+
+    async fn send(&self, frame: &Frame) -> Result<()> {
+        match self {
+            #[cfg(target_os = "linux")]
+            Self::Linux(bus) => bus.send(frame).await,
+            #[cfg(not(target_os = "linux"))]
+            _ => unreachable!(),
+        }
+    }
 }
